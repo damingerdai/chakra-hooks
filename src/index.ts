350c69d2b29b7f6f98115d340c927218af50a283
damingerdai <mingguobin@live.com>
2025-10-19 22:33:53 +0800

    feat: add useControllableState

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./use-boolean";
 export * from "./use-callback-ref";
+export * from "./use-controllable-state";
 export * from "./use-disclosure";
 export * from "./use-event-listener";
 export * from "./use-interval";
